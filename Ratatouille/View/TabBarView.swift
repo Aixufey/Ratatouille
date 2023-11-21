2436d9b65070652e820fbb36ee81d253a0c2c8b4
Rinseo <jack_11728@msn.com>
2023-11-21 14:20:36 +0100

    Adding custom tab bar view

---
+++ Ratatouille/View/TabBarView.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarView.swift
+//  Ratatouille
+//
+//  Created by Jack Xia on 21/11/2023.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        
+        TabView {
+            RecipesListView()
+                .tabItem {
+                    Label("Mine oppskrifter", systemImage: "fork.knife.circle")
+                }
+            SearchView()
+                .tabItem {
+                    Label("Søk", systemImage: "magnifyingglass.circle")
+                }
+            SettingsView()
+                .tabItem {
+                    Label("Innstillinger", systemImage: "gearshape")
+                }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
