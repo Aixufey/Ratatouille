90464618c982ffbe8029868240d8e063f7190161
Rinseo <jack_11728@msn.com>
2023-11-29 00:23:24 +0100

    Refactored codebase, using ObservableObject protocolsin SearchView

--- Ratatouille/View/RecipesListView.swift
+++ Ratatouille/View/RecipesListView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct RecipesListView: View {
+    @EnvironmentObject private var unifiedData: UnifiedModelData
+    @State private var isShowing: Bool = false
+    @State private var isRecipes: Bool = false
     var body: some View {
-        Text("Recipes List")
+        VStack {
+            VStack {
+                Image(ImageAsset.Logo.rawValue)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+                    .padding()
+                HStack {
+                    Spacer()
+                    Button {
+                        withAnimation(.easeInOut(duration: 0.13)) {
+                            isShowing.toggle()
+                        }
+                    } label: {
+                        Text("Historikk")
+                        Image(systemName: isShowing ? "arrow.down.and.line.horizontal.and.arrow.up":"arrow.up.and.line.horizontal.and.arrow.down")
+                    }
+                }.padding()
+            }
+            if isShowing {
+                Divider().padding(.horizontal)
+                Text("Søk historikk")
+                LazyVStack {
+                    SearchResultView(currentSearchResult: $unifiedData.unifiedModel)
+                        .environmentObject(IsEmptyResult().self)
+                        .environmentObject(AppSettings().self)
+                        .environmentObject(UnifiedModelData().self)
+                }.padding(.top)
+            }
+            Spacer()
+            VStack {
+                Image(systemName: "square.3.layers.3d.slash")
+                    .imageScale(.large).font(.system(size: 65))
+                Text("Ingen matoppskrifter")
+            }
+        }
     }
 }
 
 struct RecipesListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipesListView()
+            .environmentObject(IsEmptyResult().self)
+            .environmentObject(AppSettings().self)
+            .environmentObject(UnifiedModelData().self)
     }
 }
--- Ratatouille/View/SearchSubView/SearchResultView.swift
+++ Ratatouille/View/SearchSubView/SearchResultView.swift
@@ -7,32 +7,108 @@
 
 import SwiftUI
 import Kingfisher
+
+
+private struct TransformTest: Hashable {
+    var idMeal: String
+    var strMeal: String
+    var strCategory: String
+    var strArea: String
+    var strInstructions: String
+    var strMealThumb: String
+    var strYoutube: String
+}
 /**
  Subscribing to 'DVModel' and pass in id to fetch more details for each item
  */
 struct SearchResultView: View {
+    @EnvironmentObject var search: IsEmptyResult
     @Binding private var unifiedResult: UnifiedModel
     @StateObject private var DVModel = DetailViewModel()
+    @State private var hashTable = Set<[TransformTest]>()
+    @State private var API: APIService
     private let fallBackImg: String = "https://cdn-icons-png.flaticon.com/512/2276/2276931.png"
-    init(currentSearchResult unifiedResult: Binding<UnifiedModel>) {
+    init(currentSearchResult unifiedResult: Binding<UnifiedModel> = .constant(.init()), _ API: APIService = APIService.shared) {
         self._unifiedResult = unifiedResult
+        self.API = API
     }
+    private func fetchDetails(for id: String) async {
+        do {
+            let detailed = try await API.getDetails(for: id)
+            let mapped = detailed.meals?.compactMap { i in
+                return TransformTest(idMeal: i.idMeal, strMeal: i.idMeal, strCategory: i.strCategory, strArea: i.strArea, strInstructions: i.strInstructions, strMealThumb: i.strMealThumb, strYoutube: i.strYoutube)
+            } ?? []
+            hashTable.insert(mapped)
+            let uniques = Array(hashTable)
+            print("my cached obj: \(uniques.count)")
+        } catch {
+            print("Error in getting details")
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Søk")
-                .font(.custom(CustomFont.ComicBold.name, size: 35))
             NavigationView {
-                List {
-                    if let areas = unifiedResult.area?.meals {
-                        Section(header:Text("Landområde\(areas.count <= 1 ? "":"r")")) {
-                            ForEach(areas, id: \.idMeal) { area in
-                                NavigationLink {
-                                    ScrollView {
-                                        DetailView(forId: area.idMeal ?? "", usingModel: DVModel, with: $unifiedResult)
+                
+                if search.isEmpty {
+                    VStack {
+                        Spacer()
+                        KFImage(URL(string: "https://cdn-icons-png.flaticon.com/512/6134/6134065.png"))
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .frame(width: 150, height: 150)
+                            .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
+                        Text("No result")
+                        Spacer()
+                    }
+                } else {
+                    List {
+                        if let areas = unifiedResult.area?.meals {
+                            Section(header:Text("Landområde\(areas.count <= 1 ? "":"r")")) {
+                                ForEach(areas, id: \.idMeal) { area in
+                                    NavigationLink {
+                                        ScrollView {
+                                            DetailView(forId: area.idMeal ?? "", usingModel: DVModel, with: $unifiedResult)
+                                        }
+                                    } label: {
+                                        LazyHStack {
+                                            KFImage(URL(string: area.strMealThumb ?? fallBackImg))
+                                                .resizable()
+                                                .scaledToFit()
+                                                .clipShape(Circle())
+                                                .frame(width: 85, height: 85)
+                                                .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
+                                            LazyVStack(alignment: .center) {
+                                                Text(area.strMeal ?? "")
+                                                    .multilineTextAlignment(SwiftUI.TextAlignment.center)
+                                            }
+                                            .padding()
+                                            .frame(width: 200)
+                                            
+                                        }
+                                        .swipeActions(edge: .trailing) {
+                                            Button {
+                                                Task {
+                                                    await fetchDetails(for: area.idMeal ?? "")
+                                                }
+                                            } label: {
+                                                Image(systemName: "square.grid.3x1.folder.fill.badge.plus")
+                                            }.tint(.accentColor)
+                                        }
                                     }
-                                } label: {
-                                    LazyHStack {
-                                        KFImage(URL(string: area.strMealThumb ?? fallBackImg))
+                                }
+                            }
+                        }
+                        if let cats = unifiedResult.category?.meals {
+                            Section(header: Text("Kategori\(cats.count <= 1 ? "":"er")")) {
+                                ForEach(cats, id: \.idMeal) { cat in
+                                    NavigationLink {
+                                        ScrollView {
+                                            DetailView(forId: cat.idMeal, usingModel: DVModel, with: $unifiedResult)
+                                        }
+                                    } label: {
+                                        KFImage(URL(string: cat.strMealThumb ?? fallBackImg))
                                             .resizable()
                                             .scaledToFit()
                                             .clipShape(Circle())
@@ -40,7 +116,7 @@ struct SearchResultView: View {
                                             .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
                                         
                                         LazyVStack(alignment: .center) {
-                                            Text(area.strMeal ?? "")
+                                            Text(cat.strMeal)
                                                 .multilineTextAlignment(SwiftUI.TextAlignment.center)
                                         }
                                         .padding()
@@ -49,86 +125,59 @@ struct SearchResultView: View {
                                 }
                             }
                         }
-                    }
-                    if let cats = unifiedResult.category?.meals {
-                        Section(header: Text("Kategori\(cats.count <= 1 ? "":"er")")) {
-                            ForEach(cats, id: \.idMeal) { cat in
-                                NavigationLink {
-                                    ScrollView {
-                                        DetailView(forId: cat.idMeal, usingModel: DVModel, with: $unifiedResult)
+                        if let ingredients = unifiedResult.ingredient?.meals {
+                            Section(header: Text("Ingrediens\(ingredients.count <= 1 ? "":"er")")) {
+                                ForEach(ingredients, id: \.idMeal) { ing in
+                                    NavigationLink {
+                                        ScrollView {
+                                            DetailView(forId: ing.idMeal ?? "", usingModel: DVModel, with: $unifiedResult)
+                                        }
+                                    } label: {
+                                        KFImage(URL(string: ing.strMealThumb ?? fallBackImg))
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(Circle())
+                                            .frame(width: 85, height: 85)
+                                            .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
+                                        
+                                        LazyVStack(alignment: .center) {
+                                            Text(ing.strMeal ?? "")
+                                                .multilineTextAlignment(SwiftUI.TextAlignment.center)
+                                        }
+                                        .padding()
+                                        .frame(width: 200)
                                     }
-                                } label: {
-                                    KFImage(URL(string: cat.strMealThumb ?? fallBackImg))
-                                        .resizable()
-                                        .scaledToFit()
-                                        .clipShape(Circle())
-                                        .frame(width: 85, height: 85)
-                                        .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
-                                    
-                                    LazyVStack(alignment: .center) {
-                                        Text(cat.strMeal)
-                                            .multilineTextAlignment(SwiftUI.TextAlignment.center)
-                                    }
-                                    .padding()
-                                    .frame(width: 200)
                                 }
                             }
                         }
-                    }
-                    if let ingredients = unifiedResult.ingredient?.meals {
-                        Section(header: Text("Ingrediens\(ingredients.count <= 1 ? "":"er")")) {
-                            ForEach(ingredients, id: \.idMeal) { ing in
-                                NavigationLink {
-                                    ScrollView {
-                                        DetailView(forId: ing.idMeal ?? "", usingModel: DVModel, with: $unifiedResult)
+                        if let meals = unifiedResult.meal?.meals {
+                            Section(header: Text("Matrett\(meals.count <= 1 ? "":"er")")) {
+                                ForEach(meals, id: \.idMeal) { meal in
+                                    NavigationLink {
+                                        ScrollView {
+                                            DetailView(forId: meal.idMeal, usingModel: DVModel, with: $unifiedResult)
+                                        }
+                                    } label: {
+                                        KFImage(URL(string: meal.strMealThumb))
+                                            .resizable()
+                                            .scaledToFit()
+                                            .clipShape(Circle())
+                                            .frame(width: 85, height: 85)
+                                            .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
+                                        
+                                        LazyVStack(alignment: .center) {
+                                            Text(meal.strMeal)
+                                                .multilineTextAlignment(SwiftUI.TextAlignment.center)
+                                        }
+                                        .padding()
+                                        .frame(width: 200)
                                     }
-                                } label: {
-                                    KFImage(URL(string: ing.strMealThumb ?? fallBackImg))
-                                        .resizable()
-                                        .scaledToFit()
-                                        .clipShape(Circle())
-                                        .frame(width: 85, height: 85)
-                                        .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
-                                    
-                                    LazyVStack(alignment: .center) {
-                                        Text(ing.strMeal ?? "")
-                                            .multilineTextAlignment(SwiftUI.TextAlignment.center)
-                                    }
-                                    .padding()
-                                    .frame(width: 200)
                                 }
+                                
                             }
                         }
-                    }
-                    if let meals = unifiedResult.meal?.meals {
-                        Section(header: Text("Matrett\(meals.count <= 1 ? "":"er")")) {
-                            ForEach(meals, id: \.idMeal) { meal in
-                                NavigationLink {
-                                    ScrollView {
-                                        DetailView(forId: meal.idMeal, usingModel: DVModel, with: $unifiedResult)
-                                    }
-                                } label: {
-                                    KFImage(URL(string: meal.strMealThumb))
-                                        .resizable()
-                                        .scaledToFit()
-                                        .clipShape(Circle())
-                                        .frame(width: 85, height: 85)
-                                        .overlay(Circle().stroke(Color.customPrimary, lineWidth: 4))
-                                    
-                                    LazyVStack(alignment: .center) {
-                                        Text(meal.strMeal)
-                                            .multilineTextAlignment(SwiftUI.TextAlignment.center)
-                                    }
-                                    .padding()
-                                    .frame(width: 200)
-                                }
-                            }
-                            
-                        }
-                    }
-                    
-                    
-                } // List
+                    } // List
+                } // ELSE bracket
             } // Navigation
         } // VStack
     }
@@ -142,11 +191,12 @@ struct SearchResultView_Previews: PreviewProvider {
             ])
         )
         var body: some View {
-            SearchResultView(currentSearchResult: $unifiedResult)
+            SearchResultView(currentSearchResult: $unifiedResult, APIService.shared)
         }
     }
     static var previews: some View {
         Wrapper()
+            .environmentObject(IsEmptyResult().self)
             .environmentObject(AppSettings().self)
     }
 }
